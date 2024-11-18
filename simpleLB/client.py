f4923c0312dfa599593242284730e9b57237deb8
Ricardo Parizotto <ricardo.dparizotto@gmail.com>
2024-11-18 14:40:12 -0300

    client and configurations

---
+++ simpleLB/client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import random
+import socket
+import sys
+
+from scapy.all import IP, TCP, Ether, get_if_hwaddr, get_if_list, sendp
+
+def main():
+    pkt =  Ether(src=get_if_hwaddr("enp132s0f0np0"), dst='ff:ff:ff:ff:ff:ff')
+    pkt = pkt /IP(dst="10.0.1.100") / TCP(dport=1234, sport=random.randint(49152,65535)) / "teste"
+    pkt.show2()
+    sendp(pkt, iface="enp132s0f0np0", verbose=False)
+
+if __name__ == '__main__':
+    main()
